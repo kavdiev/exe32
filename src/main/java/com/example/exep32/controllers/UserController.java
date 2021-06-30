de97d8034342b71086d35c1aedf5e6a24bbad351
Karen Avdiev <k.avdiev@degroofpetercam.com>
2021-06-30 15:34:41 +0200

    some cleaning

--- src/main/java/com/example/exep32/controllers/UserController.java
+++ src/main/java/com/example/exep32/controllers/UserController.java
@@ -3,23 +3,30 @@ package com.example.exep32.controllers;
 import com.example.exep32.model.User;
 import com.example.exep32.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController
+@Transactional
+@CrossOrigin
 public class UserController {
     @Autowired
     UserRepository  userRepository;
 
 
     @GetMapping ("/users")
-    public Page<User> getAllUsers(Pageable pageable) {
-        return userRepository.findAll(pageable);
+    public List<User> getAllUsers() {
+        return (List<User>) userRepository.findAll();
     }
+
+    @GetMapping ("/users/{userId}")
+    public User getUserById(@PathParam( value ="userId") Long userId) {
+        return userRepository.findUserById(userId);
+    }
+
     @PostMapping("/users")
     public  User  addUser (@RequestBody  User user) {
         return  userRepository.save(user);
--- src/main/java/com/example/exep32/repositories/UserRepository.java
+++ src/main/java/com/example/exep32/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package com.example.exep32.repositories;
 
 import com.example.exep32.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findUserById(Long userId);
 
     //  add search by email
 }
